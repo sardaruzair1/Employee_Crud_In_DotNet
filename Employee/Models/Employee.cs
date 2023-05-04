8201544a62b6a290b755cd6d532ceb944b0cd4db
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-05-05 03:25:11 +0500

    Add project files.

---
+++ Employee/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
